481a490f3446ac8e2704680051e29554d56778e4
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-08-10 09:51:35 +0200

    added CategoryService and AdminCategoryController

---
+++ src/main/java/com/onlineshop/controller/AdminCategoryController.java
@@ -0,0 +1,48 @@
+package com.onlineshop.controller;
+
+import com.onlineshop.controller.dto.CategoryDTO;
+import com.onlineshop.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/category")
+public class AdminCategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/all")
+    public List<CategoryDTO> findAllCategory() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<CategoryDTO> addCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO category = categoryService.add(categoryDTO);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(@RequestBody CategoryDTO categoryDTO, @PathVariable Integer id) {
+        CategoryDTO category = categoryService.update(id, categoryDTO);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(category);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Integer id) {
+        CategoryDTO category = categoryService.delete(id);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(category);
+    }
+}
